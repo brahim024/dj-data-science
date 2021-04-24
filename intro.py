0a0f50ab55d19e3b3c4e6b1d4ff09c72250ad659
brahim boughanm <60741749+brahim024@users.noreply.github.com>
2021-04-25 00:54:58 +0200

    add simple function to pull data

---
+++ intro.py
@@ -0,0 +1,50 @@
+import binance.client 
+from binance.client import Client 
+
+import pandas as pd 
+import numpy as np 
+
+api_key ='9Fizq5IgCgf4CAgaYdaiht03Nue8nck7g7T2WsjBenzEwWubGbMxGXq3BB4NcsZJ'
+
+api_secret ='wWS7Dr26mTevNzTGIRe9ETMJFhsC3zkF9U0WggyFWaRNpcVLA9ygRFMKl1G6fWzk'
+
+client = Client(api_key, api_secret) 
+tickers = ['BTCUSDT','XRPUSDT','MTLUSDT']
+interval=Client.KLINE_INTERVAL_1MINUTE
+depth = '1 hours ago'
+def pulldata(ticker,interval,depth):
+		
+		Cdata = client.get_historical_klines(ticker, interval, depth)
+		print(Cdata)
+		df = pd.DataFrame(Cdata)
+		if not df.empty: 
+			
+			df[0] =  pd.to_datetime(df[0],unit='ms')
+			df.columns = ['Date','Open','High','Low','Close','Volume','IGNORE','Quote_Volume','Trades_Count','BUY_VOL','BUY_VOL_VAL','x']
+			df = df.set_index('Date')
+			
+			del df['IGNORE']    
+			del df['BUY_VOL']
+			del df['BUY_VOL_VAL']
+			del df['x']
+
+			df["Open"] = pd.to_numeric(df["Open"])
+			df ["Open"] = pd.to_numeric(df["Open"])
+			df ["High"] = pd.to_numeric(df["High"])
+			df ["Low"] = pd.to_numeric(df["Low"])
+			df ["Close"] = pd.to_numeric(df["Close"])
+			df ["Volume"] = round(pd.to_numeric(df["Volume"]))
+			df ["Quote_Volume"] = round(pd.to_numeric(df["Quote_Volume"]))
+			df ["Trades_Count"] = pd.to_numeric(df["Trades_Count"])
+			df['div']= df['Open'] / df['Close']	
+
+			df['Log_VolumeGain'] = (np.log(df["Quote_Volume"]/df.Quote_Volume.shift(1))*100).fillna(0)
+			df['pricegain'] = (df.Open.pct_change()*100).fillna(0)
+
+			df.to_csv(f'{ticker}.csv')
+
+		print(df)
+
+
+for ticker in tickers: 	
+	pulldata(ticker,interval,depth)
